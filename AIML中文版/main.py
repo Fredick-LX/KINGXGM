d62f15c2077e937c6efa063146739988220e233e
Fang Yiheng <100507147+KINGXGM@users.noreply.github.com>
2022-03-23 13:16:35 +0800

    Update main.py

--- AIML中文版/main.py
+++ AIML中文版/main.py
@@ -3,7 +3,15 @@
 import Kernel
 
 alice = Kernel.Kernel()
-alice.learn("cn-test.xml")
-
+alice.learn("语料/cn-test.txt")
+alice.learn("语料/cn-data.txt")
+alice.learn("语料/cn-history.txt")
+alice.learn("语料/cn-bye.txt")
+alice.learn("语料/cn-humor.txt")
 while True:
-    print(alice.respond(input('>>>')))
+    word = input(">>>")
+    if word in ["再见","BYEBYE","byebye","结束"]:
+        print(alice.respond(word))
+        sys.exit()
+    else:
+        print(alice.respond(word))
