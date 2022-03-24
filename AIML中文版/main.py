846058f58e921efe47565f1c2f263cce01b9aa38
Fang Yiheng <100507147+KINGXGM@users.noreply.github.com>
2022-03-24 13:24:32 +0800

    Update main.py

--- AIML中文版/main.py
+++ AIML中文版/main.py
@@ -8,7 +8,11 @@ alice.learn("语料/cn-test.txt")
 alice.learn("语料/cn-data.txt")
 alice.learn("语料/cn-history.txt")
 alice.learn("语料/cn-bye.txt")
-alice.learn("语料/cn-humor.txt")
+alice.learn("语料/cn-苏联笑话.txt")
+try:
+    alice.loadBrain("brain")
+except:
+    pass
 while True:
     word = input(">>>")
     if word in ["再见","结束"]:
