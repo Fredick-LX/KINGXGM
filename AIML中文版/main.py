482f0cf8cfca57bc41b454da75068cf8d1261c9d
Fang Yiheng <100507147+KINGXGM@users.noreply.github.com>
2022-03-25 13:17:36 +0800

    Update main.py

--- AIML中文版/main.py
+++ AIML中文版/main.py
@@ -9,15 +9,27 @@ alice.learn("语料/cn-data.txt")
 alice.learn("语料/cn-history.txt")
 alice.learn("语料/cn-bye.txt")
 alice.learn("语料/cn-苏联笑话.txt")
+alice.learn("语料/cn-emotion.txt")
+alice.learn("语料/cn-attitude.txt")
+alice.learn("语料/cn-owen.txt")
 try:
-    alice.loadBrain("brain")
+    flag = input("操作>>>(read/pass)")
+    if flag == "read":
+        flag = True
+        alice.loadBrain("brain")
+    elif flag == "pass":
+        flag = False
+        alice.saveBrain("brain")
 except:
-    pass
-while True:
+        sys.stderr.write("err\n")
+        flag = False
+while flag:
     word = input(">>>")
     if word in ["再见","结束"]:
         print(alice.respond(word))
         alice.saveBrain("brain")
-        sys.exit()
+        break
     else:
         print(alice.respond(word))
+input("按任意键退出>>>")
+sys.exit()
